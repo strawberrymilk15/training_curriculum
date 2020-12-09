11781e5ccc417f5b9bc58e22203acac15a77a47c
strawberrymilk15 <69702033+strawberrymilk15@users.noreply.github.com>
2020-12-09 15:37:28 +0900

    Issue2

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,13 +8,13 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    Plan.create(plan_params)
+    Plan.create(save_plan)
     redirect_to action: :index
   end
 
   private
 
-  def plan_params
+  def save_plan
     params.require(:calendars).permit(:date, :plan)
   end
 
